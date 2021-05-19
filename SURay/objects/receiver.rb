d0558d687b0978f9d4b056e53dedfa3aec4aa974
jcbreton <74570882+jcbreton@users.noreply.github.com>
2021-05-18 23:29:11 -0400

    calculate distance to receiver

--- SURay/objects/receiver.rb
+++ SURay/objects/receiver.rb
@@ -24,6 +24,21 @@ class Receiver
         end 
     end
 
+    def returnDistanceToIntersection(ray_origin, ray_direction)
+        if(self.check_intersection(ray_origin,ray_direction)==false)
+            return false
+        end
+
+        x1 = (ray_direction.dot(ray_origin-@position))**2 
+        x2 = (ray_direction.dot(ray_direction))*((ray_origin-@position).dot(ray_origin-@position)-(@radius**2))
+        discriminant = x1-x2
+
+        numeratorPlus = -ray_direction.dot(ray_origin-@position) + Math.sqrt(discriminant)
+        numeratorMinus = -ray_direction.dot(ray_origin-@position) - Math.sqrt(discriminant)
+
+        return [(numeratorPlus/(ray_direction.dot(ray_direction))), (numeratorMinus/(ray_direction.dot(ray_direction)))]
+    end
+
     # setters and getters
     def getName 
         return @name
