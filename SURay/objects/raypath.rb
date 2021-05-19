cbf53c990f1200196c85a7139158c14c48531c31
jcbreton <74570882+jcbreton@users.noreply.github.com>
2021-05-18 23:29:31 -0400

    ray arrival time and energy

--- SURay/objects/raypath.rb
+++ SURay/objects/raypath.rb
@@ -1,31 +1,59 @@
 require_relative 'material.rb'
+require_relative '../util/audio_math.rb'
+require 'sketchup.rb'
 
 class Raypath
     def initialize()
-        @chain = Array.new() 
+        @intersections = Array.new() 
     end
 
     def add_segment(segment)
-        @chain.push(segment)
+        @intersections.push(segment)
     end
 
     def getOrder()
-        return @chain.length()-1
+        # subtract source intersection and receiver intersection 
+        return @intersections.length()-2 
     end
 
-    def arrivalenergy(initial_energy,frequency)
+    def totalLength()
+        length = 0
+        last = (@intersections[0]).getPoint()
+
+        for i in 1..@intersections.length()-1
+            length = length + (@intersections[i].getPoint()).distance(@intersections[i-1].getPoint())
+        end
+
+        return length
+    end
+
+    def arrivalEnergy(initial_energy,frequency)
         energy = initial_energy
-        @chain.each do |i|
-            material = (i.getLayer()).getMaterial()
-            energy = energy*(1-material.getAbsAtFreq(frequency))
+        for i in 0..@intersections.length()-1
+            if (@intersections[i].getLayer() != false) 
+                material = (@intersections[i].getLayer()).getMaterial()
+                energy = energy*(1-material.getAbsAtFreq(frequency))
+            end
         end
         return energy
+    end 
+
+    def arrivalTime(sound_speed)
+        # computation in SI units
+        return (totalLength.to_m() / sound_speed)
     end
 
-    def getInfo()
-        puts "Order: #{self.getOrder()}"
+    def plot()
+        entities = (Sketchup.active_model).active_entities
+        last = (@intersections[0]).getPoint()
 
-        e = self.arrivalenergy(1,1000)
-        puts "Arrival Energy at 1000 Hz = #{e} = #{10*Math.log10(e)} dB"
+        for i in 1..@intersections.length()-1
+            entities.add_cline(@intersections[i-1].getPoint(),@intersections[i].getPoint())
+        end
+    end
+        
+    def getInfo()
+        e = self.arrivalEnergy(1,1000)
+        puts "Arrival Time: #{self.arrivalTime(343)}, Arrival Energy: #{e} (#{10*Math.log10(e)}), Order: #{self.getOrder()}"
     end
 end
